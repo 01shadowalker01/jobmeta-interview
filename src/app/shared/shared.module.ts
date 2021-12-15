56b4dd8e2710fda1e69cfdda1248c6aeb7fa2600
shadowalker <01shadowalker01@gmail.com>
2021-12-16 02:12:17 +0330

    feat: add CustomPaginator

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,21 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 
 import { FileUploaderComponent } from "./file-uploader/file-uploader.component";
+import { CustomPaginatorComponent } from "./custom-paginator/custom-paginator.component";
 
 @NgModule({
-  declarations: [FileUploaderComponent],
-  imports: [CommonModule, MatIconModule, MatProgressBarModule, MatInputModule],
-  exports: [FileUploaderComponent],
+  declarations: [FileUploaderComponent, CustomPaginatorComponent],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  exports: [FileUploaderComponent, CustomPaginatorComponent],
 })
 export class SharedModule {}
